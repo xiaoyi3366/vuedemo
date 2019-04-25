0c6e6c9d66a86c58edcb0f2fac9d00105edd2350
xiaoyi3366 <1326245718@qq.com>
2019-04-25 19:54:17 +0800

    完整的props

--- vuedemo1/src/App.vue
+++ vuedemo1/src/App.vue
@@ -6,22 +6,36 @@
       <button @click="handleClick">添加</button>
     </div>
     <ul>
+      <!-- 也可以TodoItem -->
       <todo-item v-for="(item,index) in list" :key="index">
         <template v-slot:slotItem="itemProps">
           <span :style="{fontSize:'20px',color:itemProps.checked ? 'red' : 'blue'}">{{item}}</span>
         </template>
       </todo-item>
     </ul>
+    <Props
+          name="Hello Vue！"
+          :type="type"
+          :is-visible="false"
+          :on-change="handlePropChange"
+          title="属性Demo"
+          class="test1"
+          :class="['test2']"
+          :style="{ marginTop: '20px' }"
+          style="margin-top: 10px"
+        />
   </div>
 </template>
 
 <script>
-import TodoItem from './components/TodoItem.vue'
+import TodoItem from './components/TodoItem.vue';
+import Props from './components/Props.vue'
 
 export default {
   name: 'app',
   components: {
-    TodoItem
+    TodoItem,
+    Props
   },
   data(){
     return{
