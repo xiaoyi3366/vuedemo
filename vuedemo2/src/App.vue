fcc10048da08afa01b0df2abf4d1265cf1270b2e
xiaoyi3366 <1326245718@qq.com>
2019-05-08 16:54:54 +0800

    vuex状态管理

--- vuedemo2/src/App.vue
+++ vuedemo2/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{count}}
+    <br>
+    {{$store.getters.doubleCount}}
+    <button @click="$store.commit('increment')">count++</button>
+    <button @click="$store.dispatch('increment')">actions++</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+
+    }
+  },
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- vuedemo2/src/main.js
+++ vuedemo2/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
-
+Vue.use(Vuex)
 Vue.config.productionTip = false
-
+const store = new Vuex.Store({
+  state:{
+      count: 0
+  },
+  mutations:{
+    increment(state){
+      state.count++
+    }
+  },
+  actions:{
+    increment({state}){//有state数据，不是state,类似this.store实例
+      setTimeout(()=>{
+        state.count++
+      },3000)
+    }
+  },
+  getters:{//getter起到缓存的作用
+    doubleCount(state){
+      return state.count *2
+    }
+  }
+})
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
